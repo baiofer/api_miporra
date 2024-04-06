c3d718aeba286384934a87e09b144833901b81b9
Fernando Jarilla <fjarilla@gmail.com>
2024-04-06 23:34:30 +0200

    Delete sendEmails

--- apiControllers/RecoverPasswordController.js
+++ apiControllers/RecoverPasswordController.js
@@ -1,15 +1,17 @@
-import { sendMailAWS } from "../lib/sendEmailAWS.js"
+//import { sendMailAWS } from "../lib/sendEmailAWS.js"
 
 class RecoverPasswordController {
 
 
     recoverPassword = async (req, res, next) => {
+        /*
         try {
             const result = await sendMailAWS(req.query.email, 'Cambiar su password', 'Para cambiar su password, acceda al siguiente link <a href=https://api.miporra.es/>Cambiar password</a>')
         } catch (error) {
             console.log(error)
             next(error)
         }
+        */
     }
 }
 
