f60bc63ddc8e824e21a3d21cb7c6fb3cefe18e99
Fernando Jarilla <fjarilla@gmail.com>
2024-04-06 22:53:54 +0200

    Changed AWS SES fron V2 to V3

--- apiControllers/RecoverPasswordController.js
+++ apiControllers/RecoverPasswordController.js
@@ -1,11 +1,11 @@
-import { sendMail } from "../lib/sendEmail.js"
+import { sendMailAWS } from "../lib/sendEmailAWS.js"
 
 class RecoverPasswordController {
 
 
     recoverPassword = async (req, res, next) => {
         try {
-            sendMail(req.query.email, 'Cambiar su password', 'Para cambiar su password, acceda al siguiente link <a href=https://api.miporra.es/>Cambiar password</a>')
+            const result = await sendMailAWS(req.query.email, 'Cambiar su password', 'Para cambiar su password, acceda al siguiente link <a href=https://api.miporra.es/>Cambiar password</a>')
         } catch (error) {
             console.log(error)
             next(error)
