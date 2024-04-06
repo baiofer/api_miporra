b92ab7af1b36d61ecf37e7348363364f1f898036
Fernando Jarilla <fjarilla@gmail.com>
2024-04-06 23:51:58 +0200

    Recovered sendEmailAWS

--- apiControllers/RecoverPasswordController.js
+++ apiControllers/RecoverPasswordController.js
@@ -4,14 +4,12 @@ class RecoverPasswordController {
 
 
     recoverPassword = async (req, res, next) => {
-        /*
         try {
             const result = await sendMailAWS(req.query.email, 'Cambiar su password', 'Para cambiar su password, acceda al siguiente link <a href=https://api.miporra.es/>Cambiar password</a>')
         } catch (error) {
             console.log(error)
             next(error)
         }
-        */
     }
 }
 
