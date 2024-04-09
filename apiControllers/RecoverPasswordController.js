c5a6090c5704fe04c3fdf848acec548f28ec0fda
Fernando Jarilla <fjarilla@gmail.com>
2024-04-09 11:47:25 +0200

    Added sendEmail function with "mailgun"

--- apiControllers/RecoverPasswordController.js
+++ apiControllers/RecoverPasswordController.js
@@ -1,11 +1,15 @@
-import { sendMailAWS } from "../lib/sendEmailAWS.js"
+import { sendEmail } from "../lib/sendEmail.js"
 
 class RecoverPasswordController {
 
 
     recoverPassword = async (req, res, next) => {
+        const to = 'fernando.jarilla@ikasle.aeg.eus'
+        const subject = 'Email de prueba desde miporra.es'
+        const html = '<p></p>Has solicitado resetear tu contraseña en la página miporra.es. Para realizar esta función selecciona <a href="https://miporra.es">este enlace</a></p>'
+
         try {
-            const result = await sendMailAWS(req.query.email, 'Cambiar su password', 'Para cambiar su password, acceda al siguiente link <a href=https://api.miporra.es/>Cambiar password</a>')
+            const result = await sendEmail(to, subject, html)
         } catch (error) {
             console.log(error)
             next(error)
